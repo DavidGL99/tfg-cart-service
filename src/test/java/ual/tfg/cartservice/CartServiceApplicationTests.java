265cb5928501a756a0c3852c2128b0bdb82e768e
dgomezl <david.parador99@gmail.com>
2023-06-13 09:49:13 +0200

    deploy to aws

---
+++ src/test/java/ual/tfg/cartservice/CartServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package ual.tfg.cartservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class CartServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
